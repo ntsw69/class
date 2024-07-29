926d4401531f8c1fa4ce0432f768c54811253d99
Prasiddha Acharya <acharyaprasiddha6@gmail.com>
2024-07-29 14:31:50 +0545

    hjhgjghj

---
+++ class-10-nepali/scripts-attendee.js
@@ -0,0 +1,33 @@
+import uitoolkit from 'https://class-noteswift.netlify.app/main/videosdk-ui-toolkit/index.js';
+
+var sessionContainer = document.getElementById('sessionContainer');
+var attendeeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiTnA2VTFFdXNUX0djaTFNU3J1QWF2USIsInRwYyI6IkNsYXNzIDEwIEVuZ2xpc2giLCJyb2xlX3R5cGUiOjAsInVzZXJfaWRlbnRpdHkiOiIiLCJzZXNzaW9uX2tleSI6InVuaXF1ZV9zZXNzaW9uX2lkZW50aWZpZXIiLCJ2ZXJzaW9uIjoxLCJpYXQiOjE3MjIyMTU0MjYsImV4cCI6MTcyMjM4ODIyNn0.V80bny2P_N4Ceab7TX86IXxryzJdoznS9rvsNsKyRGc';  // This will be replaced by the updateTokens.js script
+
+
+function joinSession() {
+    var userName = document.getElementById('username').value || 'Student'; // Default to 'Student' if no username is entered
+    var config = {
+        videoSDKJWT: attendeeToken,
+        sessionName: 'Class 10 Nepali',
+        userName: userName,
+        sessionPasscode: 'NTSW@#1216',
+        features: ['video', 'audio', 'settings', 'users', 'chat', 'share']
+    };
+
+    uitoolkit.joinSession(sessionContainer, config);
+
+    uitoolkit.onSessionClosed(sessionClosed);
+}
+
+var sessionClosed = (() => {
+    console.log('session closed');
+    uitoolkit.closeSession(sessionContainer);
+
+    document.getElementById('join-flow').style.display = 'block';
+});
+
+// Add event listener to button
+document.getElementById('join-button').addEventListener('click', () => {
+    document.getElementById('join-flow').style.display = 'none';
+    joinSession();
+});
