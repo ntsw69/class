9355af567e791f0db0eec94c660e5b8af508651a
Bikash <noreply.noteswift@gmail.com>
2024-08-03 16:13:25 +0545

    jghjb

---
+++ class-12-math-re-exam/scripts-host.js
@@ -0,0 +1,30 @@
+import uitoolkit from 'https://class-noteswift.netlify.app/main/videosdk-ui-toolkit/index.js';
+
+var sessionContainer = document.getElementById('sessionContainer');
+var config = {
+    videoSDKJWT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoibU41cktsSTNTLUNOZEIwTmxCSjFzZyIsInRwYyI6IkNsYXNzIDEyIE1hdGgiLCJyb2xlX3R5cGUiOjEsInVzZXJfaWRlbnRpdHkiOiJUZWFjaGVyIiwic2Vzc2lvbl9rZXkiOiJ1bmlxdWVfc2Vzc2lvbl9pZGVudGlmaWVyIiwidmVyc2lvbiI6MSwiaWF0IjoxNzIyNjQ1MTQxLCJleHAiOjE3MjI4MTc5NDF9.GSiUR-UjKw7qZg47pN-ye0BJMy0qag4KsGc1BkEsRdg',  // This will be replaced by the updateTokens.js script
+    sessionName: 'Class 12 Math Reexam',
+    userName: 'Teacher',
+    sessionPasscode: 'NTSW@#1216',
+    features: ['video', 'audio', 'settings', 'users', 'chat', 'share']
+};
+
+
+function joinSession() {
+    uitoolkit.joinSession(sessionContainer, config);
+
+    uitoolkit.onSessionClosed(sessionClosed);
+}
+
+var sessionClosed = (() => {
+    console.log('session closed');
+    uitoolkit.closeSession(sessionContainer);
+
+    document.getElementById('join-flow').style.display = 'block';
+});
+
+// Add event listener to button
+document.getElementById('join-button').addEventListener('click', () => {
+    document.getElementById('join-flow').style.display = 'none';
+    joinSession();
+});
