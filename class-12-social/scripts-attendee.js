7246010bed5fdd61f636d9f2dd55822b2a9a30fb
Prasiddha Acharya <acharyaprasiddha6@gmail.com>
2025-01-01 18:08:53 +0545

    dsdf

---
+++ class-12-social/scripts-attendee.js
@@ -0,0 +1,33 @@
+import uitoolkit from 'https://class-noteswift.netlify.app/main/videosdk-ui-toolkit/index.js';
+
+var sessionContainer = document.getElementById('sessionContainer');
+var attendeeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5Ijoib2dRaFBkcWRReXVIZzg5VlB2dTFJZyIsInRwYyI6IkNsYXNzIDEyIEVuZ2xpc2giLCJyb2xlX3R5cGUiOjAsInVzZXJfaWRlbnRpdHkiOiIiLCJzZXNzaW9uX2tleSI6InVuaXF1ZV9zZXNzaW9uX2lkZW50aWZpZXIiLCJ2ZXJzaW9uIjoxLCJpYXQiOjE3MzMzNjAzMjgsImV4cCI6MTczMzUzMzEyOH0.kN_z24Zh7A9FJjO0Oh-IiP8wjPzch_69m8C8s-0hrg0';  // This will be replaced by the updateTokens.js script
+
+
+function joinSession() {
+    var userName = document.getElementById('username').value || 'Student'; // Default to 'Student' if no username is entered
+    var config = {
+        videoSDKJWT: attendeeToken,
+        sessionName: 'Class 12 Social',
+        userName: userName,
+        sessionPasscode: 'NTSW@#1216',
+        features: ['video', 'audio', 'settings', 'users', 'chat', 'share']
+    };
+
+    uitoolkit.joinSession(sessionContainer, config);
+
+    uitoolkit.onSessionClosed(sessionClosed);
+}
+
+var sessionClosed = (() => {
+    console.log('session closed');
+    uitoolkit.closeSession(sessionContainer);
+
+    document.getElementById('join-flow').style.display = 'block';
+});
+
+// Add event listener to button
+document.getElementById('join-button').addEventListener('click', () => {
+    document.getElementById('join-flow').style.display = 'none';
+    joinSession();
+});
---
+++ class-12-social/scripts-host.js
@@ -0,0 +1,30 @@
+import uitoolkit from 'https://class-noteswift.netlify.app/main/videosdk-ui-toolkit/index.js';
+
+var sessionContainer = document.getElementById('sessionContainer');
+var config = {
+    videoSDKJWT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5Ijoib2dRaFBkcWRReXVIZzg5VlB2dTFJZyIsInRwYyI6IkNsYXNzIDEyIEVuZ2xpc2giLCJyb2xlX3R5cGUiOjEsInVzZXJfaWRlbnRpdHkiOiJUZWFjaGVyIiwic2Vzc2lvbl9rZXkiOiJ1bmlxdWVfc2Vzc2lvbl9pZGVudGlmaWVyIiwidmVyc2lvbiI6MSwiaWF0IjoxNzMzMzYwMzI4LCJleHAiOjE3MzM1MzMxMjh9.MUBifRdA9GsbWLUczX8KvWSr6RG-5kEqjBRNzzZUXX8',  // This will be replaced by the updateTokens.js script
+    sessionName: 'Class 12 Social',
+    userName: 'Teacher',
+    sessionPasscode: 'NTSW@#1216',
+    features: ['video', 'audio', 'settings', 'users', 'chat', 'share']
+};
+
+
+function joinSession() {
+    uitoolkit.joinSession(sessionContainer, config);
+
+    uitoolkit.onSessionClosed(sessionClosed);
+}
+
+var sessionClosed = (() => {
+    console.log('session closed');
+    uitoolkit.closeSession(sessionContainer);
+
+    document.getElementById('join-flow').style.display = 'block';
+});
+
+// Add event listener to button
+document.getElementById('join-button').addEventListener('click', () => {
+    document.getElementById('join-flow').style.display = 'none';
+    joinSession();
+});
